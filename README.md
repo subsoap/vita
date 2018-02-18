e379963e1c052442454199e98ce576d7599bdfe6
Brian Kramer <subsoap@gmail.com>
2018-02-18 06:42:11 -0800

    Adding chrono

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Vita
 A generic energy / life system for Defold
+
+## Installation
+You can use Vita in your own project by adding this project as a [Defold library dependency](http://www.defold.com/manuals/libraries/). Open your game.project file and in the dependencies field under project add:
+
+	https://github.com/subsoap/vita/archive/master.zip
+
+Once added, you must require the main Lua module in scripts via
+
+```
+local vita = require("vita.vita")
+```
+
+## Tips
+Some games use energy. Some use hearts. Which should you use? Generally an energy system is better because it allows you to price events in different energy amounts while users may not like that events cost multiple hearts. But some games doesn't need that price flexibility and so can use a simple heart system where one heart is one attempt at a level.
--- vita/vita.lua
+++ vita/vita.lua
@@ -1,7 +1,57 @@
 local defsave = require("defsave.defsave")
+local chrono = require("chrono.chrono")
 
 local M = {}
 
+M.resources = {}
+
 M.defsave_filename = "vita" -- filename to use with defsave
+M.defsave_key = "vita"
+M.initiated = false
+M.vita_data_filename = "/vita/blank_data.lua"
+
+function M.update(dt)
+	if M.initiated == false then
+		M.init()
+	end
+end
+
+function M.init()
+	
+	defsave.load(M.defsave_filename)
+	M.resources = defsave.get(M.defsave_filename, M.defsave_key)
+	M.initiated = true
+end
+
+function M.final()
+	M.save()
+end
+
+-- You should save data with vita.save() whenever you do normal game saves
+function M.save()
+	M.update_defsave()
+	defsave.save_all()	
+end
+
+-- You can update data into DefSave without saving right now
+function M.update_defsave()
+	defsave.set(M.defsave_filename, M.defsave_key, M.resources)
+end
+
+-- Add an amount to a resource tag, if not exist it's created
+function M.add(tag, amount)
+end
+
+-- Consumes an amount
+function M.consume(tag, amount)
+end
+
+-- Gets the total amount of a resource tag available
+function M.get(tag)
+end
+
+-- Regenerates a resource tag by its regenerate amount
+function M.regenerate(tag)
+end
 
 return M
